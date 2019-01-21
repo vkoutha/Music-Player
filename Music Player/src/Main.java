59928f1c96867534dfcfdf7cb61b3b36498ed0ac
Vedh Koutha <vkoutha@gmail.com>
2019-01-21 01:27:15 -0500

    Removed KeyListener

--- Music Player/src/Main.java
+++ Music Player/src/Main.java
@@ -4,7 +4,7 @@ import java.awt.event.KeyListener;
 
 import javax.swing.JFrame;
 
-public class Main implements KeyListener{
+public class Main{
 	
 	public static Frame frame;
 	
@@ -19,26 +19,4 @@ public class Main implements KeyListener{
 		
 	}
 
-	@Override
-	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	public static void main(String[] args) {
-		new Main();
-	}
-	
 }
